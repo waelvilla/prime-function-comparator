322d87573644df4feaa8e945c3d9bf90e6d101ce
waelvilla <waelvilla@gmail.com>
2020-03-11 09:42:02 +0300

    added formulas layout

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
-console.log("Hi")
+/*
+Resources: 
+https://en.wikipedia.org/wiki/Formula_for_primes
+
+
+Formulas: 
+1- https://wikimedia.org/api/rest_v1/media/math/render/svg/91340957e8616e6cdb438a0bede7e4ba804e4922
+
+
+*/
+
+const formulas= [
+    {
+        name: "Prime Polynomial Function",
+        formula: (n)=> Math.pow(n,2) + n + 41
+    },
+    {
+        name: "f2",
+        formula: (n)=> {}
+    },
+    {
+        name: "f3",
+        formula: (n)=> {}
+    },
+    {
+        name: "f4",
+        formula: (n)=> {}
+    },
+    {
+        name: "f5",
+        formula: (n)=> {}
+    },
+    {
+        name: "f6",
+        formula: (n)=> {}
+    },
+    {
+        name: "f7",
+        formula: (n)=> {}
+    }
+]
+
+function getFirstTenThousandPrimes(){
+    let primeNumbers= []
+    for( var i =0; i<10000; i++){
+        //run 
+    }
+    return primeNumbers
+}
+
