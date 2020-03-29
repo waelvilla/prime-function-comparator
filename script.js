ad7e135ebde815e65d6ee5213d4f20460a252d3d
waelvilla <waelvilla@gmail.com>
2020-03-29 23:58:11 +0300

    formulas dynamic

--- script.js
+++ script.js
@@ -9,73 +9,37 @@ Formulas:
 
 */
 
-//Formulas to be tested
-const formulas = [
-    {
-        name: "Prime Polynomial Function",
-        formula: (n) => Math.pow(n, 2) + n + 41
-    },
-    {
-        name: "f2",
-        formula: (n) => { }
-    },
-    {
-        name: "f3",
-        formula: (n) => { }
-    },
-    {
-        name: "f4",
-        formula: (n) => { }
-    },
-    {
-        name: "f5",
-        formula: (n) => { }
-    },
-    {
-        name: "f6",
-        formula: (n) => { }
-    },
-    {
-        name: "f7",
-        formula: (n) => { }
-    }
-]
+window.onload= run()
 
-// checks if a number is prime by its factors from 2 to the square root of the number
-function isPrime(number) {
-    let maxDivisor = Math.ceil(Math.sqrt(number))
-    for (var i = 2; i <= maxDivisor; i++) {
-        if (number % i == 0) {
-            return false
-        }
-    }
-    return true
+
+function setFunction(number){
+    console.log("ff ", number);
+    document.getElementById('dropdownTitle').innerHTML= number
 }
 
-// Gets first X number of prime numbers starting from 1 
-let getFirstXPrimes = (count) => {
-    let primeNumbers = []
-    for (var i = 1; i < count; i++) {
-        if (isPrime(i)) {
-            primeNumbers.push(i)
-        }
-    }
-    return primeNumbers
+function checkPrime(){
+    const userInput= document.querySelector('#userInput')
+console.log(userInput.value)
 }
 
-const FIRST_TEN_THOUSAND_PRIMES=getFirstXPrimes(10000);
 
-formulas.map((formulaObject)=>{
-    let startTime= new Date()
-    let primeNumbers=[]
-    for(var i =0; i<10000; i++){
-        let number= formulaObject.formula(i)
-        if(number && isPrime(number)){
-            primeNumbers.push(number)
-        }
-    }
-    let endTime= new Date()
-    let timeElapsed= (endTime - startTime) / 1000
-    // console.log(primeNumbers);
-    console.log("formula: ", formulaObject.name, "took", timeElapsed, "seconds");
-})
+function run(){
+    let dropdown= document.querySelector('#dropdownContent')
+
+    
+    formulas.map(({name})=>{
+        var anchor = document.createElement('a')
+        anchor.onclick=`return setFunction(${name})`
+        anchor.innerHTML= name
+        dropdown.appendChild(anchor)
+    })
+
+
+
+
+}
+function createLink(image) {
+  let cards = document.getElementsByClassName('cards')
+
+
+}
